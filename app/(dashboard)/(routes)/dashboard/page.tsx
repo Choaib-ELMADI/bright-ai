d85e2e339163cbef5c24e68659386a3ea6d9f24f
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-07-26 18:03:42 +0100

    Folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <p className="text-3xl">Dashboard (Protected)</p>
+            <Button variant="outline">outline</Button>
+        </div>
+    )
+};
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function LandingPage() {
+    return (
+        <div>
+            <p className="text-3xl">Landing (Unprotected)</p>
+            <Button variant="outline">outline</Button>
+        </div>
+    )
+};
