e082784ff0b4b50b6b14457f5570893917152a0e
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-07-28 00:20:08 +0100

    Image generation is done

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,10 +1,58 @@
-import { ImageIcon } from 'lucide-react';
+'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Download, ImageIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import Image from 'next/image';
+import axios from 'axios';
+import * as z from 'zod';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { amountOptions, formSchema, resolutionOptions } from './constants';
+import { Card, CardFooter } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Loader } from '@/components/loader';
 import Heading from "@/components/heading";
+import { Empty } from '@/components/empty';
 
 
 
 export default function ImagePage() {
+    const router = useRouter();
+    const [images, setImages] = useState<string[]>([]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: '',
+            amount: '1',
+            resolution: '512x512'
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {          
+            setImages([]);
+
+            const response = await axios.post('/api/image', values);
+
+            const urls = response.data.map((image: { url: string }) => image.url);
+            setImages(urls);
+            
+            form.reset();
+        } catch (error: any) {
+            console.log(error);
+        } finally {
+            router.refresh();
+        };
+    };
+
     return (
         <div>
             <Heading
@@ -14,6 +62,143 @@ export default function ImagePage() {
                 iconColor='text-pink-700'
                 bgColor='bg-pink-700/10'
             />
+            <div className='px-4 lg:px-8'>
+                <div>
+                    <Form { ...form }>
+                        <form 
+                            onSubmit={ form.handleSubmit(onSubmit) }
+                            className='rounded-lg border w-full py-2 px-3 focus-within:shadow-sm grid grid-cols-12 gap-2'
+                        >
+                            <FormField
+                                name='prompt'
+                                render={({ field }) => (
+                                    <FormItem className='col-span-12 lg:col-span-6'>
+                                        <FormControl className='p-0 m-0'>
+                                            <Input 
+                                                className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent w-full'
+                                                disabled={ isLoading }
+                                                placeholder='A fish swimming in space'
+                                                { ...field }
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+
+
+                            <FormField 
+                                control={ form.control }
+                                name='amount'
+                                render={({ field }) => (
+                                    <FormItem className='col-span-12 md:col-span-4 lg:col-span-2'>
+                                        <Select 
+                                            disabled={ isLoading }
+                                            onValueChange={ field.onChange }
+                                            value={ field.value }
+                                            defaultValue={ field.value }
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={ field.value } />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {
+                                                    amountOptions.map((option) => (
+                                                        <SelectItem key={ option.value } value={ option.value }>
+                                                            { option.label }
+                                                        </SelectItem>
+                                                    ))
+                                                }
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+
+
+                            <FormField 
+                                control={ form.control }
+                                name='resolution'
+                                render={({ field }) => (
+                                    <FormItem className='col-span-12 md:col-span-4 lg:col-span-2'>
+                                        <Select 
+                                            disabled={ isLoading }
+                                            onValueChange={ field.onChange }
+                                            value={ field.value }
+                                            defaultValue={ field.value }
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={ field.value } />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {
+                                                    resolutionOptions.map((option) => (
+                                                        <SelectItem key={ option.value } value={ option.value }>
+                                                            { option.label }
+                                                        </SelectItem>
+                                                    ))
+                                                }
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+
+
+                            <Button 
+                                className='col-span-12 md:col-span-4 lg:col-span-2 w-full'
+                                disabled={ isLoading }
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className='space-y-4 mt-4'>
+                    { isLoading && (
+                        <div className='p-20'>
+                            <Loader />
+                        </div>
+                    )}
+                    { images.length === 0 && !isLoading && (
+                        <Empty
+                            label='Turn your prompts into images'
+                        />
+                    )}
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8'>
+                        {
+                            images.map((src) => (
+                                <Card 
+                                    key={ src }
+                                    className='rounded-lg overflow-hidden'
+                                >
+                                    <div className='relative aspect-square'>
+                                        <Image 
+                                            fill
+                                            src={ src }
+                                            alt='Generated image'
+                                            draggable='false'
+                                        />
+                                    </div>
+                                    <CardFooter className='p-2'>
+                                        <Button 
+                                            onClick={ () => window.open(src) }
+                                            variant='secondary' 
+                                            className='w-full'
+                                        >
+                                            <Download className='w-4 h-4 mr-4' />
+                                            Download
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- components/loader.tsx
+++ components/loader.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const Loader = () => {
     return (
         <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-            <div className="w-10 h-10 relative animate-spin">
+            <div className="w-10 h-10 relative animate-bounce">
                 <Image 
                     fill
                     src='/logo-trs.png'
